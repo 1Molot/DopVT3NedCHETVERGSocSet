1bfdffb63fb6d15531f83e81000324e51cb48b2e
1Molot <sinkevich1990@gmail.com>
2023-04-13 12:16:25 +0300

    dop-socset-vt-3ned-chetverg

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import './App.css';
+import {NavBar} from "./components/Nav/NavBar";
+import {Profile} from "./components/Profile/Profile";
+import {Dialogs} from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+import {News} from "./components/News/News";
+import {Music} from "./components/Music/Music";
+import {Settings} from "./components/Settings/Settings";
+import {Header} from "./components/Header/Header";
+import {
+    ActionsTypes,
+    DialogsDataType,
+    MessagesDataType,
+    PostDataType,
+    StorePropsType,
+    StoreType,
+} from "./redux/State";
+
+export type AppPropsType = {
+    store: StorePropsType
+    dispatch: (action: ActionsTypes) => void
+
+}
+
+function App(props: AppPropsType) {
+    const state = props.store.getState()
+    return (
+        <BrowserRouter>
+
+            <div className="App">
+                <Header/>
+                <div className={"navAndContent"}>
+                    <NavBar/>
+
+                    <div className={'content'}>
+                        <Route
+                            path={"/dialogs"}
+                            render={() => <Dialogs
+                                dialogsData={state.dialogPage.dialogsData}
+                                messagesData={state.dialogPage.messagesData}
+                                newMessageBody={state.dialogPage.newMessageBody}
+                                dispatch={props.dispatch}
+                            />}/>
+                        <Route path={"/profile"}
+                               render={() => <Profile
+                                   newPostText={state.profilePage.newPostText}
+                                   dispatch={props.dispatch}
+                                   profilePage={state.profilePage.posts}/>}/>
+                        <Route path={"/news"} render={() => <News/>}/>
+                        <Route path={"/music"} render={() => <Music/>}/>
+                        <Route path={"/settings"} render={() => <Settings/>}/>
+                    </div>
+                </div>
+            </div>
+
+        </BrowserRouter>);
+}
+
+export default App;
---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from './Profile.module.css'
+import MyPosts from "../MyPosts/MyPosts";
+import {ProfileInfo} from "./ProfileInfo";
+import {ActionsTypes, PostDataType} from "../../redux/State";
+
+export type ProfilePropsType={
+    profilePage:PostDataType[]
+    newPostText:string
+    dispatch: (action: ActionsTypes) => void
+
+
+}
+export const Profile = (props:ProfilePropsType) => {
+    return (
+        <div >
+            <ProfileInfo/>
+            <MyPosts  profilePage={props.profilePage}
+                      dispatch={props.dispatch}
+                      newPostText={props.newPostText}
+
+            />
+        </div>
+    )
+};
